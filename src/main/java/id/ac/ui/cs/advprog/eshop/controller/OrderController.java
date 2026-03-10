91d2ae72994522f997f3d8a059567c6de517fe8f
ForgerPhysics <sihalohoryan2@gmail.com>
2026-03-10 20:23:55 +0700

    [RED] Add OrderControllerTest and OrderController skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+    // Sengaja dikosongkan untuk fase RED
+}
